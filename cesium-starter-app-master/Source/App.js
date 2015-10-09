d6438b03059532ea706453f88abfc2896370c462
Taka. NAKAYAMA <tak.izumi.564@gmail.com>
2015-10-09 12:41:40 +0900

    App edited

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,9 +1,10 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 
-//‹“_‚Ì•ÏX
+//ï¿½ï¿½ï¿½_ï¿½Ì•ÏX
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.76,35.67, 15000.0)});
 
 
-//kml“Ç‚İ‚İ
+//kmlï¿½Ç‚İï¿½ï¿½ï¿½
 viewer.dataSources.add(Cesium.KmlDataSource.load('http://yunie564.github.io/cesium-handson/cesium-starter-app-master/Source/test.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://yunie564.github.io/cesium-handson/cesium-starter-app-master/Source/DriveRoute.kml'));
